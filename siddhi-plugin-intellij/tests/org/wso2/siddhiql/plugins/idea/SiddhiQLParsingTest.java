2dfa438f1111f8461f19dce2e3747385a5b69ac8
vihanga-liyanage <vihangaliyanage007@gmail.com>
2017-04-03 14:27:23 +0530

    Add test data and test project

---
+++ siddhi-plugin-intellij/tests/org/wso2/siddhiql/plugins/idea/SiddhiQLParsingTest.java
@@ -0,0 +1,28 @@
+package org.wso2.siddhiql.plugins.idea;
+
+import com.intellij.testFramework.ParsingTestCase;
+
+public class SiddhiQLParsingTest extends ParsingTestCase {
+  public SiddhiQLParsingTest() {
+    super("", "SiddhiQL", new SiddhiQLParserDefinition());
+  }
+
+  public void testParsingTestData() {
+    doTest(true);
+  }
+
+  @Override
+  protected String getTestDataPath() {
+    return "testData";
+  }
+
+  @Override
+  protected boolean skipSpaces() {
+    return false;
+  }
+
+  @Override
+  protected boolean includeRanges() {
+    return true;
+  }
+}
